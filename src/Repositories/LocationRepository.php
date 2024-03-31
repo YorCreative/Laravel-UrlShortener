bd7f8ef79c0db8253e67d1948bcd7287e83302a6
Dariush Vesal <dariush.vesal@gmail.com>
2024-03-31 09:41:32 +0330

    Fix getLocationForm, throw error Cannot use object of type

--- src/Repositories/LocationRepository.php
+++ src/Repositories/LocationRepository.php
@@ -39,14 +39,14 @@ class LocationRepository
     {
         $clickLocation = Location::get($ip);
 
-        if (! $clickLocation) {
+        if (!$clickLocation) {
             return LocationRepository::locationUnknown($ip);
         }
 
-        $clickLocation['longitude'] = (float) $clickLocation['longitude'];
-        $clickLocation['latitude'] = (float) $clickLocation['latitude'];
+        $clickLocation->longitude = (float) $clickLocation->longitude;
+        $clickLocation->latitude = (float) $clickLocation->latitude;
 
-        unset($clickLocation['driver']);
+        unset($clickLocation->driver);
 
         return $clickLocation->toArray();
     }
