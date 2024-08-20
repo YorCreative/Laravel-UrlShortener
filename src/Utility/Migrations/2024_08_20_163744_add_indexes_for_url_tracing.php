6cec16c3114ca656f5bb8cd029d031f1d2ff728e
Yorda <yordadevs@gmail.com>
2024-08-20 11:46:44 -0600

    :fire:

--- src/Utility/Migrations/2024_08_20_163744_add_indexes_for_url_tracing.php
+++ src/Utility/Migrations/2024_08_20_163744_add_indexes_for_url_tracing.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,7 +15,13 @@ return new class extends Migration
     public function up()
     {
         Schema::table('short_url_tracings', function (Blueprint $table) {
-            $table->index(['utm_source(100)', 'utm_medium(100)', 'utm_campaign(100)', 'utm_content(100)', 'utm_term(100)'], 'utm_composite_index');
+            DB::statement('CREATE INDEX utm_composite_index ON short_url_tracings (utm_source(100), utm_medium(100), utm_campaign(100), utm_content(100), utm_term(100))');
+        });
+    }
+
+    public function down(){
+        Schema::table('short_url_tracings', function (Blueprint $table) {
+            $table->dropIndex('utm_composite_index');
         });
     }
 };
